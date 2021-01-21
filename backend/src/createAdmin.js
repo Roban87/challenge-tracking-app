b23a51949bdb856afe3e0ba7ae9f335b112c1fd4
kisborg <mark.borgstrom@gmail.com>
2021-01-21 18:09:45 +0100

    fixed create admin script

--- backend/src/createAdmin.js
+++ backend/src/createAdmin.js
@@ -1,3 +1,3 @@
 import { registerService } from './services/register.service';
 
-registerService.insertNewUser('admin', 'password', true).then(() => process.exit());
+registerService.insertNewUser('admin', 'password', 'admin@email.com', true).then(() => process.exit());
